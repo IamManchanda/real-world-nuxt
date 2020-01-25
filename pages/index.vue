e830849781bdc857b3c9defa70ff975af65c10e0
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-01-25 15:18:11 +0530

    Starter app: Real world Nuxt!

---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <section class="container">
+    <div>
+      <logo/>
+      <h1 class="title">
+        real-world-nuxt
+      </h1>
+      <h2 class="subtitle">
+        Real World Nuxt.js
+      </h2>
+      <div class="links">
+        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
+        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import Logo from "~/components/Logo.vue";
+
+export default {
+  components: {
+    Logo,
+  },
+};
+</script>
