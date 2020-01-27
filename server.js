4dd4c39ee9fd41e02ef4659510a02c064c7ce0d3
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-01-27 15:08:47 +0530

    Chore: Move server code to root directory.

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3000;
 app.set("port", port);
 
 // Import and Set Nuxt.js options
-const config = require("../nuxt.config.js");
+const config = require("./nuxt.config.js");
 config.dev = !(process.env.NODE_ENV === "production");
 
 const bootFrontend = async () => {
